ffa1774b4c3096f0b11309d8a4fc7f6ff6b72736
Your Name <you@example.com>
2023-04-02 16:47:10 +0545

    Setup MVVM Architecture and fetch data from server and display data in UI.

---
+++ WalEAssignment/WalEAssignment/Common/Common.swift
@@ -0,0 +1,53 @@
+//
+//  Common.swift
+//  WalEAssignment
+//
+//  Created by Mac on 02/04/2023.
+//
+
+import Foundation
+
+struct Constants {
+
+    static let ErrorAlertTitle = "Error"
+    static let OkAlertTitle = "Ok"
+    static let CancelAlertTitle = "Cancel"
+    static let CalenderImage = "calender"
+    static let NoImage = "no-image-icon"
+    static let ApodDateFormat = "yyyy-MM-dd"
+    static let MediaTypeImage = "image"
+    static let CachedResponseKey = "CachedResponse"
+
+    enum MediaType {
+        case image
+    }
+
+    static func getAPIKey() -> String {
+
+        let config = getConfig()
+        return config?["APOD_KEY"] as? String ?? ""
+    }
+
+    static func getApodBaseURL() -> String {
+
+        let config = getConfig()
+        return config?["BASE_URL"] as? String ?? ""
+    }
+
+    static func getConfig() -> [String: Any]? {
+
+        var config: [String: Any]?
+
+        if let infoPlistPath = Bundle.main.url(forResource: "Info", withExtension: "plist") {
+            do {
+                let infoPlistData = try Data(contentsOf: infoPlistPath)
+                if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: Any] {
+                    config = dict
+                }
+            } catch {
+                print(error)
+            }
+        }
+        return config
+    }
+}
